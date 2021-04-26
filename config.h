a7bc02aa34ab003377ba3a0db882b9421c193881
PierrotAWB <andrewwang298@gmail.com>
2021-04-26 14:25:37 -0400

    Apply modified 'vanitygaps' patch

--- config.h
+++ config.h
@@ -1,8 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
@@ -11,11 +16,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_bg[]          = "#005577";
+static const char col_border[]      = "#08a0c9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2  },
+	[SchemeSel]  = { col_gray4, col_bg,    col_border },
 };
 
 /* tagging */
@@ -56,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_bg, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "brave", NULL };
 
@@ -76,6 +82,16 @@ static Key keys[] = {
 	{ Mod1Mask,                     XK_h,      setmfact,       {.f = -0.05} },
 	{ Mod1Mask,                     XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,             			XK_z, 	   zoom,           {0} },
+	{ MODKEY|ShiftMask,     		XK_h, 	   zoom,           {0} },
+	{ MODKEY|ShiftMask,    			XK_l, 	   zoom,           {0} },
+	{ MODKEY,             			XK_u, 	   incrgaps,       {.i = +2} },
+	{ MODKEY|ShiftMask,    			XK_u, 	   incrgaps,       {.i = -2} },
+	{ MODKEY,             			XK_i, 	   incrigaps,      {.i = +2} },
+	{ MODKEY|ShiftMask,    			XK_i, 	   incrigaps,      {.i = -2} },
+	{ MODKEY,             			XK_o, 	   incrogaps,      {.i = +2} },
+	{ MODKEY|ShiftMask,    			XK_o, 	   incrogaps,      {.i = -2} },
+	{ MODKEY|ShiftMask,    			XK_g, 	   togglegaps,     {0} },
+	{ MODKEY|ShiftMask,    			XK_equal,  defaultgaps,    {0} },
 	{ MODKEY,             			XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
